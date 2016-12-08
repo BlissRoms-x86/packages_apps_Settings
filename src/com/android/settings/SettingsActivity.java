ea73bdfd89b4f109d808ab438ad10dc4791d3c24
Unknown <electrikjesus@gmail.com>
2016-12-08 14:31:57 -0500

    Settings: [2/3] Power Menu config
    also ressurect custom button settings
    
    Based on Change-Id: I139fe462fc7d325f53b3d3c43e1d6a5d81338727

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -137,6 +137,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
+import org.omnirom.omnigears.ButtonSettings;
+
 public class SettingsActivity extends SettingsDrawerActivity
         implements PreferenceManager.OnPreferenceTreeClickListener,
         PreferenceFragment.OnPreferenceStartFragmentCallback,
@@ -357,6 +359,7 @@ public class SettingsActivity extends SettingsDrawerActivity
             WifiInfo.class.getName(),
             MasterClear.class.getName(),
             NightDisplaySettings.class.getName(),
+            ButtonSettings.class.getName(),
     };
 
 
@@ -548,6 +551,7 @@ public class SettingsActivity extends SettingsDrawerActivity
 
         mIsShowingDashboard = className.equals(Settings.class.getName())
                 || className.equals(Settings.WirelessSettings.class.getName())
+                || className.equals(Settings.ButtonSettings.class.getName())
                 || className.equals(Settings.DeviceSettings.class.getName())
                 || className.equals(Settings.PersonalSettings.class.getName())
                 || className.equals(Settings.WirelessSettings.class.getName());
