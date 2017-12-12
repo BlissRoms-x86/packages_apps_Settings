cd7af65a4180fcb39e798092118c33e53735f94f
Chih-Wei Huang <cwhuang@linux.org.tw>
2017-12-13 01:23:34 +0800

    [port to pie] Add information about OpenGL driver version
    In order to get Mesa / OpenGL ES info, an EGL context is required.
    Without it, GLES20.glGetString returns NULL.
    
    Conflicts:
    	res/xml/device_info_settings.xml
    	src/com/android/settings/DeviceInfoSettings.java
    
    Conflicts:
    	src/com/android/settings/system/SystemDashboardFragment.java

--- src/com/android/settings/deviceinfo/DeviceInfoSettings.java
+++ src/com/android/settings/deviceinfo/DeviceInfoSettings.java
@@ -33,6 +33,7 @@ import com.android.settings.deviceinfo.imei.ImeiInfoPreferenceController;
 import com.android.settings.deviceinfo.simstatus.SimStatusPreferenceController;
 import com.android.settings.deviceinfo.UpdatePreferenceController;
 import com.android.settings.deviceinfo.DeviceManufacturerPreferenceController;
+import com.android.settings.deviceinfo.OpenGLVersionPreferenceController;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settings.search.Indexable;
 import com.android.settingslib.core.AbstractPreferenceController;
@@ -141,6 +142,7 @@ public class DeviceInfoSettings extends DashboardFragment implements Indexable {
         controllers.add(new FccEquipmentIdPreferenceController(context));
 	    controllers.add(new UpdatePreferenceController(context));
         controllers.add(new DeviceManufacturerPreferenceController(context));
+        controllers.add(new OpenGLVersionPreferenceController(context));
         controllers.add(new BuildNumberPreferenceController(context, activity, fragment, lifecycle));
         return controllers;
     }
